87ce778453f0cded6de74e44cc9660e5c0741a16
Luca Fortin <luca.f.fortin@gmail.com>
2026-08-24 14:43:18 -0300

    FEAT: EJ 9 RECURSION

--- Recursion/9_logaritmo.py
+++ Recursion/9_logaritmo.py
@@ -1,5 +1,21 @@
-def logaritmo_recursivo(base, exp):
-    if (exp == 1): return 0
-    else: return 1 + logaritmo_recursivo(base, exp / base)
+import sys
+from pathlib import Path
 
-print(logaritmo_recursivo(2, 8))
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from utils import limpiar
+from validaciones import validar_numero
+import math
+
+
+def logaritmo_recursivo(base, exp):
+    if (exp <= 1): return 0
+    else: return 1 + logaritmo_recursivo(base, math.floor(exp / base))
+
+# main
+limpiar()
+
+base = validar_numero("Ingrese el numero base: ")
+exponente = validar_numero("Ingrese el exponente de la base: ")
+
+print(f"El logaritmo en base {base} de {exponente} es igual a: {logaritmo_recursivo(base, exponente)}")
