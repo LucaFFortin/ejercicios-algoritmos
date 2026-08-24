d6e54c55100b418f051f53377500f7f1d3ddb02e
Luca Fortin <luca.f.fortin@gmail.com>
2026-08-24 14:43:12 -0300

    FEAT: EJ 7 RECURSION

--- Recursion/7_serie_numerica.py
+++ Recursion/7_serie_numerica.py
@@ -1,5 +1,20 @@
-def serie_recursiva(numero):
-    if (numero == 1): return 1
-    else: return numero / serie_recursiva(numero - 1)
+import sys
+from pathlib import Path
 
-print(serie_recursiva(2))
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from utils import limpiar
+from validaciones import validar_numero
+
+def serie_recursiva(numero):
+    if (numero < 0): numero = abs(numero)
+    if (numero > 100): return "Valor maximo excedido, solo puede ingresar un numero menor o igual a 100"
+    if (numero <= 1): return numero
+    else: return serie_recursiva(numero - 1) + 1 / numero
+
+# main
+limpiar()
+
+numero = validar_numero("Ingrese el valor final de la serie armonica: ")
+
+print(f"Con el valor final {numero}, la serie armonica queda asi: {serie_recursiva(numero=numero)}.")
