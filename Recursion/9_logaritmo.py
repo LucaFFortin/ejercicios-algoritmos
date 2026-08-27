dc8419b08b2237a56257bf4ffa9e0fc8f4d64f05
Luca Fortin <luca.f.fortin@gmail.com>
2026-08-27 10:48:02 -0300

    Fix: add math constrains of a logarithm

--- Recursion/9_logaritmo.py
+++ Recursion/9_logaritmo.py
@@ -9,6 +9,10 @@ import math
 
 
 def logaritmo_recursivo(base, exp):
+    if (base <= 0 or base == 1 or exp <= 0):
+        print("Por definicion, la base debe ser mayor a 0 y diferente de uno, el exponente debe ser mayor a 0")
+        return
+    
     if (exp <= 1): return 0
     else: return 1 + logaritmo_recursivo(base, math.floor(exp / base))
 
