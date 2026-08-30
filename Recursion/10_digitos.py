02f2d86a75f181914f2318306c7c291f7ae4f561
Luca Fortin <luca.f.fortin@gmail.com>
2026-08-30 09:35:12 -0300

    Fix: remove prints

--- Recursion/10_digitos.py
+++ Recursion/10_digitos.py
@@ -13,16 +13,6 @@ def contador_digitos_recursivo(num):
     if (num > 9):
         return 1 + contador_digitos_recursivo(math.floor(num / 10))
     else: return 1
-    
-print(contador_digitos_recursivo(1))
-print(contador_digitos_recursivo(9))
-print(contador_digitos_recursivo(-1))
-print(contador_digitos_recursivo(10))
-print(contador_digitos_recursivo(100))
-print(contador_digitos_recursivo(1000))
-print(contador_digitos_recursivo(10000))
-print(contador_digitos_recursivo(100000))
-print(contador_digitos_recursivo(1000000))
 
 # main
 limpiar()
