fce197d42059276f9c66569ec5a0d91195adceb9
Luca Fortin <luca.f.fortin@gmail.com>
2026-08-30 10:09:04 -0300

    Feat: ex 19 resolved

---
+++ Recursion/19_sucesion_recursiva.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from utils import limpiar
+from validaciones import validar_string, validar_numero
+
+
+def sucesion_recursiva(num):
+    if (num == 1): return 2
+    else:
+        return (1 / sucesion_recursiva(num - 1)) + num
+
+
+# main
+limpiar()
+
+numero = validar_numero("Ingrese un numero para calcular la sucesion: ")
+
+print(f"La sucesion recursiva de {numero} es igual a: {sucesion_recursiva(numero)}")
